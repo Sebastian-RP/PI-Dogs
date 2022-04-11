30a67ee6db3a2d80f1f020f1e20209014bdd7ef0
BUFFON <55423389+Sebastian-RP@users.noreply.github.com>
2022-04-10 22:42:11 -0500

    dandole estilo al header del componente Home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -12,7 +12,8 @@ import {
 import Card from "../Card/Card";
 import Paginate from "../Paginate/Paginate";
 import SearchBar from "../SearchBar/SearchBar"
-// import style from "../Home/Home.modules.css";
+
+import style from "../Home/Home.module.css"
 
 function Home() {
   const dispatch = useDispatch();
@@ -56,58 +57,71 @@ function Home() {
     setOrden(`Ordenado ${e.target.value}`);
   };
 
-  
+  console.log("inicio")
+  console.log(currentDogs)
+  console.log("inicio-cierre");
 
+  // ***************** renderizado ***********
+  // ***************** renderizado ***********
   return (
     <>
-      <select onChange={handleOrderByName}>
-        <option disabled defaultValue>
-          Alphabetical order
-        </option>
-        <option value="A-Z">A-Z</option>
-        <option value="Z-A">Z-A</option>
-      </select>
+      <header className={`${style.header}`}>
+        <div className={`${style.header_container_left}`}>
+          <SearchBar />
+          <div className={`${style.header_left}`}>
+            <select onChange={handleOrderByName}>
+              <option disabled defaultValue>
+                Alphabetical order
+              </option>
+              <option value="A-Z">A-Z</option>
+              <option value="Z-A">Z-A</option>
+            </select>
 
-      <select onChange={handleOrderByWeight}>
-        <option disabled defaultValue>
-          Filter by weight
-        </option>
-        <option value="max_weight">Max</option>
-        <option value="min_weight">Min</option>
-      </select>
+            <select onChange={handleOrderByWeight}>
+              <option disabled defaultValue>
+                Filter by weight
+              </option>
+              <option value="max_weight">Max</option>
+              <option value="min_weight">Min</option>
+            </select>
 
-      <select onChange={handleFilterByTemperament}>
-          <option disabled defaultValue>Temperaments</option>
-          <option value="Todos">All</option>
-          {
-            allTemperaments?.map(temp => (
-                <option value={temp.name}  key={temp.id}>{temp.name}</option>
-            ))
-          }
-      </select>
-
-      <SearchBar />
+            <select onChange={handleFilterByTemperament}>
+                <option disabled defaultValue>Temperaments</option>
+                <option value="Todos">All</option>
+                {
+                  allTemperaments?.map(temp => (
+                      <option value={temp.name}  key={temp.id}>{temp.name}</option>
+                  ))
+                }
+            </select>
+          </div>
+        </div>
+        
+        <div className={`${style.header_right}`}>
+          <Link to="/dog">CREATE DOG</Link>
+        </div>
+      </header>
+    <div className={style.main_container}>
       
-      <div className="">
-        <Link to="/dog">CREATE DOG</Link>
-      </div>
-      
-
-      <div className="container-cards">
-        <div>
-          {currentDogs?.map((el) => {//validacion que existan los datos
-            return(
+      <div className={style.container_cards}>
+        {currentDogs?.map((el) => {//validacion que existan los datos
+          return(
+            <div className={`${style.container_card}`}>
               <Link to={"/dog-detail/"+el.id} key={el.id}>
-                <Card key={el.id} image={el.image} name={el.name} temperaments={el.temperaments[0].name ? el.temperaments.map(el => el.name) : el.temperaments}/>
-              </Link>
-            )
-          })}
+              {
+                !el.temperaments[0] //en caso de no tener temperamentos establecidos
+                ? el.temperaments[0] = "no-temperaments"
+                : <Card key={el.id} image={el.image} name={el.name} temperaments={el.temperaments[0].name ? el.temperaments.map(el => el.name) : el.temperaments}/>
+                //si temperaments viene en un formato distinto desde la BD
+              }
+            </Link>
+            </div>      
+          )
+        })}
         </div>
 
-      </div>
-      {console.log(currentDogs)}
-
       <Paginate dogsPerPage={dogsPerPage} allDogs={allDogs.length} paginado={paginado}/> {/*el valor de la funcion de paginado aumenta segun el bucle for en el componente Paginate*/}
+    </div>
     </>
   );
 }
